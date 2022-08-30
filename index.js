a99857092acc5bf291cee5184885791062a1a827
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-08-30 12:27:53 -0400

    add inquirer prompts for engineer, intern, and manager

---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+let team = [];
+
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?"
+        },
+        {
+            type: 'input',
+            name: 'ID',
+            message: "What is the engineer's ID number?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub username?"
+        }
+    ])
+}
+
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'ID',
+            message: "What is the intern's ID number?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email?"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the name of the intern's school?"
+        }
+    ])
+}
+
+const addManager = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'ID',
+            message: "What is the manager's ID number?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email?"
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is the manager's office number?"
+        }
+    ])
+}
+
